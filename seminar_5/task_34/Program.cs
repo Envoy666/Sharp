dbf085bdc9bbeefc6ad3c6417a9482153f25869a
Envoy666 <etherlord285@gmail.com>
2023-08-03 14:39:50 +0300

    seminar 5

---
+++ seminar_5/task_34/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[10];
+Random rand = new();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+}
+
+int count = 0;
+
+foreach (int i in array)
+{
+    Console.Write($"{i} ");
+    if ((i & 1) == 0) count++;
+}
+Console.WriteLine();
+
+Console.WriteLine(count);
