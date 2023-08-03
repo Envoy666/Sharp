60239f81136037326a4dae7d0123c30dbe5036d2
Envoy666 <etherlord285@gmail.com>
2023-08-03 21:36:30 +0300

    seminar 7

---
+++ seminar_7/task_47/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+Random rand = new();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Math.Round((rand.NextDouble() * 20) - 10, 2);
+    }
+}
+
+WriteTable(array, null);
+
+
+static void WriteTable(double[,] table, string? header)
+{
+    if (header != null) Console.WriteLine(header);
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write("{0, 5} ", table[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
