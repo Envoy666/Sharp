226406adeed50cb814fa2ebf88519aae281dc710
Envoy666 <etherlord285@gmail.com>
2023-08-03 20:28:55 +0300

    seminar 6

---
+++ seminar_6/task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите количество чисел:");
+int common = Convert.ToInt32(Console.ReadLine());
+int positive = 0;
+
+Console.WriteLine("Введите числа:");
+while (common > 0)
+{
+    if (Convert.ToInt32(Console.ReadLine()) > 0) positive++;
+    common--;
+}
+Console.WriteLine();
+Console.WriteLine(positive);
