4e1daf62f4abaac56a7756219e4f96b3e04c4c7d
Envoy666 <etherlord285@gmail.com>
2023-08-03 13:44:01 +0300

    seminar 3

---
+++ seminar_3/task_21/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Точка A:");
+int[] coordA = Input_Coord();
+Point A = new(coordA[0], coordA[1], coordA[2]);
+
+Console.WriteLine("Точка B:");
+int[] coordB = Input_Coord();
+Point B = new(coordB[0], coordB[1], coordB[2]);
+
+double distance = Math.Sqrt(Math.Pow(A.x - B.x, 2)
+                            + Math.Pow(A.y - B.y, 2)
+                            + Math.Pow(A.z - B.z, 2));
+
+
+Console.WriteLine("расстояние: " + distance);
+
+static int[] Input_Coord()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите три координаты через пробел:");
+        string? input_str = Console.ReadLine();
+        if (input_str == null) continue;
+        string[] coordinates = input_str.Split();
+        if (coordinates.Length < 3) continue;
+
+        int x = Convert.ToInt32(coordinates[0]);
+        int y = Convert.ToInt32(coordinates[1]);
+        int z = Convert.ToInt32(coordinates[2]);
+        return new[] { x, y, z };
+    }
+}
+
+
+class Point
+{
+    public readonly int x;
+    public readonly int y;
+    public readonly int z;
+    public Point(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ seminar_3/task_23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+while (current <= number)
+{
+    Console.Write(Math.Pow(current, 3));
+    if (current != number) Console.Write(", ");
+    current++;
+}
