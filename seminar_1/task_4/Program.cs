b41d33cc40f97baa70f6dc89b37dcf9c6ed9c7ee
Envoy666 <etherlord285@gmail.com>
2023-08-03 13:29:20 +0300

    seminar 1

---
+++ seminar_1/task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int temp = Convert.ToInt32(Console.ReadLine());
+if (temp > max) max = temp;
+
+Console.WriteLine("Введите третье число:");
+temp = Convert.ToInt32(Console.ReadLine());
+if (temp > max) max = temp;
+
+Console.WriteLine("max = " + max);
---
+++ seminar_1/task_8/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число:");
+int i = Convert.ToInt32(Console.ReadLine());
+
+for (int j = 2; j <= i; j += 2) Console.Write(j + " ");
