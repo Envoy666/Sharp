785c1773d9a98cfbddea394106fd026a64e8bc4d
Envoy666 <etherlord285@gmail.com>
2023-08-03 13:37:59 +0300

    seminar 2

---
+++ seminar_2/task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = (int)Math.Log10(num) + 1;
+
+if (count < 3) Console.WriteLine("третьей цифры нет");
+else
+{
+    count -= 3;
+    num /= (int)Math.Pow(10, count);
+
+    Console.WriteLine(num % 10);
+}
+
+
