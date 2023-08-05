012ac451697160439a8dfb9e412162251c66f5c9
Envoy666 <etherlord285@gmail.com>
2023-08-05 17:58:29 +0300

    seminar 9

---
+++ seminar_9/task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < n) WriteEven(m, n);
+else WriteEven(n, m);
+
+
+static void WriteEven(int m, int n)
+{
+    if ((m & 1) == 0)
+    {
+        Console.Write(m);
+        if (m + 1 < n) Console.Write(", ");
+    }
+    if (m != n) WriteEven(m + 1, n);
+}
---
+++ seminar_9/task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < n) Console.WriteLine(NaturalSum(m, n));
+else Console.WriteLine(NaturalSum(n, m));
+
+
+static int NaturalSum(int m, int n)
+{
+    // с учетом возможного ввода отрицательных значений
+    if (m == n) return m < 0 ? 0 : m;
+    return (m < 0 ? 0 : m) + NaturalSum(m + 1, n);
+}
